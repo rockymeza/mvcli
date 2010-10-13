b4ba424a5f7367119ac795eb613eec29834f75ca
Gavin Wahl <gwahl@fusionbox.com>
2010-10-13 15:18:33 -0600

    Convert Config tests from doctest to unittest

--- core/config.py
+++ core/config.py
@@ -1,46 +1,19 @@
 class Config:
     """
     Config Class is a container for the configuration settings that the core classes use
-    >>> config = Config()
     """
     def __init__(self, dictionary = None):
         """
         Instantiates an instance of the Config class.
 
         It can accept a dictionary for the default configurations
-
-        >>> config1 = Config()
-        >>> config2 = Config({'key1': 'value1', 'key2': 'value2'})
-        >>> config2['key1']
-        'value1'
-        >>> config2['nonexistent_key'] == None
-        True
-        >>> config2.default('key1', 'other value')
-        >>> config2['key1']
-        'value1'
-        >>> config2['key2'] = 'new value'
-        >>> config2['key2']
-        'new value'
-
-        # a tricky one
-        >>> config1 = Config()
-        >>> config2 = Config()
-        >>> config1[1] = 2
-        >>> config2[1] == None
-        True
         """
         self.dictionary = dictionary or {}
 
     def __getitem__(self, key):
         """
-        Returns a value from the config dictionary based on the key
-        >>> config['x'] = 1
-        >>> config['x']
-        1
-
-        If that key does not exist in the dictionary, it will return None
-        >>> config[12] == None
-        True
+        Returns a value from the config dictionary based on the key. If that
+        key does not exist in the dictionary, it will return None
         """
         return self.dictionary.get(key)
 
@@ -49,10 +22,6 @@ class Config:
         Stores a value in the dictionary using a key
         
         If a value for that key already exists, it will be overwritten
-        >>> config['y'] = 1
-        >>> config['y'] = 2
-        >>> config['y']
-        2
         """
         self.dictionary[key] = value
 
@@ -61,15 +30,6 @@ class Config:
         Stores a value in the dictionary using a key
         
         If a value for that key already exists, it will not be overwritten
-        >>> config.default('x', 1)
-        >>> config['x']
-        1
-        >>> config['y'] = 2
-        >>> config['y']
-        2
-        >>> config.default('y', 'other value')
-        >>> config['y']
-        2
         """
         if not key in self.dictionary:
             self.dictionary[key] = value
@@ -80,13 +40,6 @@ class Config:
         You can change the name if you want
      
         Will overwrite by default
-        >>> config1 = Config({'a': 1, 'b': 2})
-        >>> config1.merge({'b': 3, 'c': 4})
-        >>> config1['b']
-        3
-        >>> config1.merge({'c': 5, 'd': 6}, False)
-        >>> config1['c']
-        4
         """
         if overwrite:
             self.dictionary.update(dictionary)
--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,57 @@
-import doctest
+import unittest
 import sys
+import random
 
 sys.path.extend(['..', '.'])
-import core.config
+from core.config import Config
 
-doctest.testmod(core.config)
+
+class TestConfig(unittest.TestCase):
+    def test_retrieval(self):
+        config = Config()
+        config[1] = 42
+        self.assertEquals(config[1], 42)
+        config['key'] = 'value'
+        self.assertEquals(config['key'], 'value')
+
+    def test_overwrite(self):
+        config = Config()
+        config[1] = 1
+        config[1] = 2
+        self.assertEquals(config[1], 2)
+
+    def test_init(self):
+        config = Config({'key1': 'value1', 'key2': 'value2'})
+        self.assertEquals(config['key1'], 'value1')
+        self.assertEquals(config['nonexistent_key'], None)
+
+    def test_default(self):
+        config = Config()
+        config['key'] = 'value'
+        config.default('key', 'wrong value')
+        config.default(1, 2)
+        self.assertEquals(config['key'], 'value')
+        self.assertEquals(config[1], 2)
+
+    def test_init_empty_dict(self):
+        config1 = Config()
+        config2 = Config()
+        config1[1] = 2
+        self.assertEquals(config2[1], None)
+
+    def test_merge(self):
+        config = Config({'a': 1, 'b': 2})
+        config.merge({'b': 3, 'c': 4})
+        self.assertEquals(config['b'], 3)
+        self.assertEquals(config['a'], 1)
+        config.merge({'c': 5, 'd': 6}, False)
+        self.assertEquals(config['c'], 4)
+
+    def test_merge_overwrite(self):
+        config = Config({1: 1})
+        config.merge({1: 2}, False)
+        self.assertEquals(config[1], 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
