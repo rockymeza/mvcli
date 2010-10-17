4a4c0f77bc2af7285bff61df16e5ff7d0d3db347
Rocky Meza <rockymeza@gmail.com>
2010-10-16 21:33:58 -0600

    new way to register documentation

--- controller/help.py
+++ controller/help.py
@@ -1,19 +1,18 @@
 from core import introspection
 from core.formatter import color
+from collections import namedtuple
+from core.exceptions import *
+
+Metadata = namedtuple('Metadata', 'description options examples')
 
 class Help:
-    title = 'Help Controller'
-    description = 'This does not do anything'
+    actions = {}
 
     def __init__(self, request, config):
         self.request = request
         self.config = config
-        self.actions = {}
     
     def main(self):
-        """
-        Description: Prints app-wide help
-        """
         print 'Hello World'
 
     def foo(self, bar, baz, qux, quux):
@@ -22,16 +21,16 @@ class Help:
     def help(self, *actions):
         if actions:
             for action in actions:
-                if hasattr(self, action):
+                if action in self.actions:
                     method = getattr(self, action)
                     out = []
                     out.append(color(self.title + '#' + action, 'cyan'))
-                    meta = introspection.getmeta(method)
+                    meta = self.actions[action]
 
                     for attr in ['description']:
-                        if attr in meta:
+                        if hasattr(meta, attr):
                             out.append(attr.upper())
-                            out.append('\t' + meta[attr])
+                            out.append('\t' + getattr(meta, attr))
                     
                     print '\n'.join(out)
                 else:
@@ -45,18 +44,24 @@ class Help:
                     out.append(attr.upper())
                     out.append('\t' + getattr(self, attr))
             
-            actions = introspection.getactions(self)
-            if actions:
+            if self.actions:
                 out.append('SUBCOMMANDS:')
-                for name, method in actions.items():
+                for name, method in self.actions.items():
                     line = '\t' + color(name, 'yellow')
-                    meta = introspection.getmeta(method)
-                    if 'description' in meta:
-                        line += '\t' + meta['description']
+                    meta = self.actions[name]
+                    for attr in ['description']:
+                        if hasattr(meta, attr):
+                            line += '\t\t' + getattr(meta, attr)
                     out.append(line)
                     
             print '\n'.join(out)
-        
-        
-        
 
+def action(cls, name, description, examples=None, options=None):
+    cls.actions[name] = Metadata(description, options, examples or [])
+    
+        
+        
+Help.title = 'Help Controller'
+Help.description = 'This is the awesome help controller'
+action(Help, 'main', 'this is main')
+action(Help, 'foo', 'foo description')
